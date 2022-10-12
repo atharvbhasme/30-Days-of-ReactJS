84a14cb1200e0c8b89dd4f9bb5ccae676d140962
atharv <atharvbhasme@gmail.com>
2022-10-12 14:07:43 +0530

    new file added

---
+++ Day 3 Requirement/imagelink/src/usercard.js
@@ -0,0 +1,47 @@
+import profileimg from "./img/resume.jpg";
+export const Usercards = () => {
+  return (
+    <section className="cardsection">
+      <img className="profileimg" src={profileimg} />
+      <div className="name-title">
+        <h3>ATHARV BHASME</h3>
+        <p>Web Developer Fresher</p>
+      </div>
+      <h3>SKILLS</h3>
+      <div className="allskill">
+        <section className="skills">
+          <h3>{skilllist[0]}</h3>
+        </section>
+        <section className="skills">
+          <h3>{skilllist[1]}</h3>
+        </section>
+        <section className="skills">
+          <h3>{skilllist[2]}</h3>
+        </section>
+        <section className="skills">
+          <h3>{skilllist[3]}</h3>
+        </section>
+        <section className="skills">
+          <h3>{skilllist[4]}</h3>
+        </section>
+        <section className="skills">
+          <h3>{skilllist[5]}</h3>
+        </section>
+        <section className="skills">
+          <h3>{skilllist[6]}</h3>
+        </section>
+      </div>
+      <p>🕤 Joined on 30 Jun 2020</p>
+    </section>
+  );
+};
+
+const skilllist = [
+  "HTML",
+  "CSS",
+  "JAVASCIPRT",
+  "MONGODB",
+  "JAVA",
+  "NODEJS",
+  "GITHUB",
+];
